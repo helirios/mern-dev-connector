59e8085a154a5545a8100ffb445913f4e2e6acab
Heli Ríos <hi@helirios.com>
2019-04-30 00:39:06 +0200

    User registration logic

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get("mongoURI");
 const connectDb = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useCreateIndex: true
     });
 
     console.log("MongoDB connected...");
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Database connection
 connectDb();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running..."));
 
 // Define routes
